ee9afaa9a7f4d86dc3b3cf527e5638ba3d876fc0
ZERO <chapon-c1@outlook.jp>
2022-01-24 00:16:22 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace WpfApp2;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : MetroWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private SubWindow _subWindow;
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (_subWindow is null)
+        {
+            _subWindow = new();
+            _subWindow.Owner = this;
+        }
+        _subWindow.Show();
+    }
+}
---
+++ WpfApp2/SubWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace WpfApp2;
+/// <summary>
+/// SubWindow.xaml の相互作用ロジック
+/// </summary>
+public partial class SubWindow : MetroWindow
+{
+    public SubWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        e.Cancel = true;
+        this.Visibility = Visibility.Hidden;
+    }
+}
